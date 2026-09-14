3673f4d53ab1eeff14d605e197e42f28c1d877e6
Adrian Alberto Espinosa Bran <adrianalbertoespinosabran@gmail.com>
2026-09-14 06:27:53 -0600

    Factor 200

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -5,5 +5,10 @@ b = 30
 c = 30 *5
 
 print ('resultado=',c)
-d = c * 100
-print ('resultadototal=',d)
+
+## cambiado el factor 102según requerimiento del negocio
+## cambiado el factor a 110 según junta de control
+## cambio a factor 200 por Directorio
+
+d = c * 200
+print ('resultado total=',d)
